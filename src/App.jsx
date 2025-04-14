9219d21e160f8b8afeeae4dc35ff00f40d9fbf9f
Aaron <thatshakespeare2@gmail.com>
2025-04-14 17:28:13 +0100

    foot

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { fetchStopCode, fetchLiveArrivals } from "./util/api.js";
 import BusArrival from "./components/BusArrival";
 import InputForm from "./components/InputForm.jsx";
 
-
+import Footer from "./components/Footer.jsx";
 function App() {
   const [arrivals, setArrivals] = useState([]);
   const [stopInfo, setStopInfo] = useState(null);
@@ -33,19 +33,24 @@ function App() {
   };
 
   return (
-    <div className="container flex flex-col justify-center items-center my-10 mx-auto max-w-2xl p-8 rounded-xl shadow-2xl">
-      <div className="flex flex-col  justify-between items-center gap-4">
-        <h1 className="text-2xl text-center font-bold tracking-wider">Live Bus Times</h1>
+    <>
+    <div className='container flex flex-col justify-center items-center my-10 mx-auto max-w-2xl p-8 rounded-xl shadow-2xl'>
+      <div className='flex flex-col  justify-between items-center gap-4'>
+        <h1 className='text-2xl text-center font-bold tracking-wider'>
+          Live Bus Times
+        </h1>
         <InputForm onSubmit={handleStopCodeSubmit} />
-      <p className="text-sm text-center md:text-left">Enter the 5-digit code found at the bus stop</p>
-
+        <p className='text-sm text-center md:text-left'>
+          Enter the 5-digit code found at the bus stop
+        </p>
       </div>
-      {loading && <p className="text-blue-600 my-6 text-center">Loading...</p>}
-      {error && <p className="text-red-600 my-6 text-center">{error}</p>}
-      {stopInfo && (
-        <BusArrival stopInfo={stopInfo} arrivals={arrivals} />
-      )}
+      {loading && <p className='text-blue-600 my-6 text-center'>Loading...</p>}
+      {error && <p className='text-red-600 my-6 text-center'>{error}</p>}
+      {stopInfo && <BusArrival stopInfo={stopInfo} arrivals={arrivals} />}
     </div>
+      <Footer />
+
+</>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className='absolute bottom-4 w-full flex items-center justify-center'>
+        <a href='https://linktr.ee/thatshakespeare' target='_blank' 
+        className="font-special-gothic tracking-widest text-xs text-accent-1 font-semibold hover:opacity-75 transition-opacity duration-500 ease-in-out">
+        by Aaron H Shakespeare
+        </a>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
